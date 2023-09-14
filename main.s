db6fa19cf54c83ab1f691fa27d8684d93a389b92
anto <antoalasepp@gmail.com>
2023-09-14 22:16:15 +0300

    add two numbers and save it to memory. Then print out the value

--- main.s
+++ main.s
@@ -5,20 +5,26 @@
 
 .section .data
 
-output:
-    .ascii "Hello"
-world:
-    .ascii "World\n"
 output_number:
     .ascii "%d\n\0"
-output_end:
-    .ascii ""
+first_number:
+    .quad 3
+second_number:
+    .quad 5
+result:
+    .quad 0
+
 
 .section .text
 main:
+    movq first_number, %rax
+    addq second_number, %rax
+    movq %rax, result
+
+print_output:
     movq stdout, %rdi
     movq $output_number, %rsi   # prints out a number format
-    movq $5, %rdx               # the number to print
+    movq result, %rdx               # the number to print
     movq $0, %rax               # rax must be zeroed out. Rax might be used for floating point numbers
     call fprintf
 
