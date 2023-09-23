20ed7ab9e7bb911ac9aab213cef79f0aad7d18d3
anto <antoalasepp@gmail.com>
2023-09-24 00:01:28 +0300

    basic obstacle print out

--- game.s
+++ game.s
@@ -5,14 +5,31 @@
 
 .section .data
 
+#IO texts
 start_game:
     .ascii "Start game? y/n\n\0"
 input_start_game:
     .ascii "%c\0"
-output_next:
+print_next:
     .ascii "game begins here\n\0"
-output_exit:
+print_round_begin:
+    .ascii "round nr %d\n\0"
+print_round_obstacle:
+    .ascii "round obstacle %d with object type %d\n\0"
+print_exit:
     .ascii "exit game\n\0"
+
+obstacles:
+    .quad 1, 4
+    .quad 2, 5
+    .quad 3, 6
+level:
+    .quad 1, 1, 2, 3, 0
+obstacle_index:
+    .quad 0
+active_obstacle:
+    .quad 0
+
 .section .text
 
 main:
@@ -31,13 +48,33 @@ main:
     cmpq $'y', %rax
     jne complete
 
-
 game_next:
-    leaq output_next, %rdi
+    leaq print_next, %rdi
+    call print_output
+game_loop:
+    #print new round info 
+    leaq print_round_begin, %rdi
+    movq obstacle_index, %rdx
+    call print_output
+
+
+    #update the active obstacle
+    leaq obstacles, %r9
+    leaq level, %rbx
+    movq $2, %r8
+    movq (%rbx, %r8, 8), %rax
+    movq %rax, active_obstacle
+
+    # print obstacle in round
+    leaq print_round_obstacle, %rdi
+    movq active_obstacle, %rdx
+
+    #get the obstacle nr from obstacle list
+    movq (%r9, %rdx, 8), %rcx #%rdx has the active obstacle number
     call print_output
 
 complete:
-    leaq output_exit, %rdi
+    leaq print_exit, %rdi
     call print_output
 
     movq $0, %rax               # rax should be zeroed out
